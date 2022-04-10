bb2aeed91c229e416656a16ebfc8ee070d60294f
temo mikava <temsonmikva@gmail.com>
2022-04-10 19:00:58 +0400

    added data context and connection string

---
+++ HumansGuide.Infrastructure.Persistance/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using HumansGuide.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansGuide.Infrastructure.Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Human> Humans { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+    }
+}
---
+++ HumansGuide.Infrastructure.Persistance/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansGuide.Infrastructure.Persistance
+{
+    public static class ServiceExtensions
+    {
+        public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(options => options.UseSqlServer(configuration.GetConnectionString("MyConnection")));
+        }
+    }
+}
