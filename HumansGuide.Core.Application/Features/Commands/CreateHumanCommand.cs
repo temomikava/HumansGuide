86309f5227b8a9bc4c9919b43138e94f6214ab13
temo mikava <temsonmikva@gmail.com>
2022-04-11 01:55:41 +0400

    implementation MediatR and SQRS pattern

---
+++ HumansGuide.Core.Application/Features/Commands/CreateHumanCommand.cs
@@ -0,0 +1,31 @@
+﻿using HumansGuide.Core.Domain.Enums;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HumansGuide.Core.Application.Features.Commands
+{
+    public class CreateHumanCommand
+    {
+        public class Request : IRequest
+        {
+            public string FisrtName { get; set; }
+            public string LastName { get; set; }
+            public GenderType Gender { get; set; }
+            public string PersonalNo { get; set; }
+            public DateTime DarteOfBirth { get; set; }
+            public string Image { get; set; }
+        }
+        public class Handler : IRequestHandler<Request>
+        {
+            public async Task<Unit> Handle(Request request, CancellationToken cancellationToken)
+            {
+                return Unit.Value;
+            }
+        }
+    }
+}
---
+++ HumansGuide.Core.Application/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansGuide.Core.Application
+{
+    public static class ServiceExtensions
+    {
+        public static void AddApplicatonLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
