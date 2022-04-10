ccb34c65ad6802f2ad8b3c9d59e233ca3b4b6202
temo mikava <temsonmikva@gmail.com>
2022-04-11 01:18:30 +0400

    implementing unit of work pattern

---
+++ HumansGuide.Core.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansGuide.Core.Application.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IHumanRepository HumanRepository { get; }
+    }
+}
---
+++ HumansGuide.Infrastructure.Persistance/Implementations/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using HumansGuide.Core.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumansGuide.Infrastructure.Persistance.Implementations
+{
+    public class UnitOfWork:IUnitOfWork
+    {
+        private IHumanRepository humanRepository;
+
+        private readonly DataContext context;
+        public UnitOfWork(DataContext context) => this.context = context;
+
+        //public IHumanRepository HumanRepository => humanRepository ??= new HumanRepository(context);
+        public IHumanRepository HumanRepository
+        {
+            get
+            {
+                return humanRepository ??= new HumanRepository(context);
+            }
+        }
+    }
+}
